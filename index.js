9b2d227e14a07d0437e9d695277980edbad80f85
manoti3 <hillarymanoti0@gmail.com>
2024-01-10 22:53:10 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+function divide(){
+    num1 = 2000;
+    num2 = 100;
+    return num1/num2
+               
+}
+function square(){
+
+
+
+
+
+}
+function add(){
+    let parameter1 = 2;
+    let parameter2 = 3;
+    const add =(parameter1, parameter2) => parameter1 + parameter2;
+
+}
